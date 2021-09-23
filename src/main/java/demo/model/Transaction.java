0627bbe99cfbf398a9a7b952ec08a1936205e53c
saikiran <skiran252@gmail.com>
2021-09-23 23:26:56 +0530

    changes in transaction.java

--- src/main/java/demo/model/Transaction.java
+++ src/main/java/demo/model/Transaction.java
@@ -95,11 +95,11 @@ public class Transaction {
         this.category = category;
     }
 
-	public String getCategory_name() {
-		return category_name;
-	}
+    public String getCategory_name() {
+        return category_name;
+    }
 
-	public void setCategory_name(String category_name) {
-		this.category_name = category_name;
-	}
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
 }
